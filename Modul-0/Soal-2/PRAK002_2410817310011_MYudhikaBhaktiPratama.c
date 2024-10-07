ef04b59ad2c627244cacefd5d08138656b046ff2
YudhikaPratama26 <yudhikapratama26@gmail.com>
2024-10-07 10:29:06 +0800

    Add files via upload

---
+++ Modul-0/Soal-2/PRAK002_2410817310011_MYudhikaBhaktiPratama.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Selamat Pagi, M.Yudhika Bhakti Pratama \n");
+    printf("Selamat Siang, M.Yudhika Bhakti Pratama \n");
+    printf("Selamat Malam, M.Yudhika Bhakti Pratama \n");
+    return 0;
+}
---
+++ Modul-0/Soal-2/PRAK002_2410817310011_MYudhikaBhaktiPratama.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, M.Yudhika Bhakti Pratama")
+print("Selamat Siang, M.Yudhika Bhakti Pratama")
+print("Selamat Malam, M.Yudhika Bhakti Pratama")
